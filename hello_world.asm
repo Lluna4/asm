57ed82077d00146bc057f73f981d6c649c5dd516
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-05-14 21:14:21 +0000

    a

--- hello_world.asm
+++ hello_world.asm
@@ -1,1 +1,19 @@
+.global _start
+.intel_syntax noprefix
 
+_start:
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 14
+    lea rsi, [hello_world]
+    syscall
+    
+    //sys_exit
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+hello_world:
+    .asciz "Hello world!\n"
