643b3940c74dd11eb65791ef2dfe6b2f42625db0
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-05-16 00:17:37 +0000

    qwqwqw11111dds

--- input.asm
+++ input.asm
@@ -4,52 +4,59 @@
 input_buffer:
     .skip 20
 
+num_buffer:
+    .skip 4
+
 .section .text
 .globl _start
 _start:
-
-    // Print a message to prompt the user for input
     mov rax, 1
     mov rdi, 1
     mov rdx, 111
     lea rsi, [inicio]
     syscall
 
-    // Read input from the user into the input buffer
     mov rax, 0
     mov rdi, 0
-    mov rdx, 256
+    mov rdx, 1
     lea rsi, [input_buffer]
     syscall
 
-    // Compare the input with the string "1"
-    mov rcx, 2
+    mov rcx, 1
     lea rdi, [one]
     lea rsi, [input_buffer]
     repe cmpsb
     je  uno
 
-    // Exit the program with a status of 0
     mov rax, 60
     xor rdi, rdi
     syscall
 
 uno:
-    // Print a message to prompt the user for more input
     mov rax, 1
     mov rdi, 1
     mov rdx, 68
     lea rsi, [unos]
     syscall
 
-    // Read more input from the user into the input buffer
     mov rax, 0
-    mov rdi, 0    
-    mov rdx, 256
-    lea rsi, [input_buffer]
+    mov rdi, 0
+    mov rdx, 1
+    lea rsi, [num_buffer]
     syscall
 
-    // Exit the program with a status of 0
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    lea rsi, [num_buffer]
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 4
+    lea rsi, [son]
+    syscall
+
     mov rax, 60
     xor rdi, rdi
     syscall
@@ -59,6 +66,12 @@ inicio:
 
 unos:
     .asciz "Has elegido cambiar de euros a dolares, dime cuantos euros tienes: "
+
+son:
+    .asciz "€ son"
+
+dolar:
+    .asciz "$"
 
 one:
     .asciz "1"
